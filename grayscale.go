c017a73a4d87bcab1004dd68622ea3d30945e932
AmCh-Q <AmCh-Q@users.noreply.github.com>
2022-04-06 04:23:19 +0800

    Add files via upload

---
+++ grayscale.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+    "fmt"
+    "bytes"
+    "strings"
+    "sync"
+    "os"
+    "os/exec"
+    "io/ioutil"
+)
+
+func worker(filename string) {
+    fmt.Println("Start converting segment", filename)
+    cmd1 := exec.Command("sudo", "ffmpeg", "-y", "-i", "temp/"+filename, "-vf", "hue=s=0", "-c:v", "libx264", "-pix_fmt", "yuv420p", "-c:a", "copy", "temp/_"+filename)
+    var stdout bytes.Buffer
+    var stderr bytes.Buffer
+    cmd1.Stdout = &stdout
+    cmd1.Stderr = &stderr
+    if (cmd1.Run() != nil) {
+        fmt.Fprintln(os.Stderr, stderr.String())
+    }
+    
+    fmt.Println("Done converting segment", filename)
+    
+    err := os.Remove("temp/"+filename)
+    if err != nil {
+        fmt.Fprintln(os.Stderr, err)
+    }
+    err = os.Rename("temp/_"+filename, "temp/"+filename)
+    if err != nil {
+        fmt.Fprintln(os.Stderr, err)
+    }
+}
+
+func main() {
+    filename := os.Args[1]
+    cmd1 := exec.Command("sudo", "ffmpeg", "-y", "-i", filename, "-f", "segment", "-segment_list", "temp/list.ffcat", "-segment_time", "10", "-reset_timestamps", "1", "-c", "copy", "temp/output_%03d.mp4")
+    var stdout bytes.Buffer
+    var stderr bytes.Buffer
+    cmd1.Stdout = &stdout
+    cmd1.Stderr = &stderr
+    if (cmd1.Run() != nil) {
+        fmt.Fprintln(os.Stderr, stderr.String())
+    }
+    
+    cmd2 := exec.Command("sudo", "chmod", "-R", "777", ".")
+    stdout.Reset()
+    stderr.Reset()
+    cmd2.Stdout = &stdout
+    cmd2.Stderr = &stderr
+    if (cmd2.Run() != nil) {
+        fmt.Fprintln(os.Stderr, stderr.String())
+    }
+    
+    var wg sync.WaitGroup
+    items, _ := ioutil.ReadDir("./temp")
+    for _, item := range items {
+        name := item.Name()
+        if !strings.HasPrefix(name,"_") && strings.HasSuffix(name, ".mp4") {
+            wg.Add(1)
+            go func() {
+                defer wg.Done()
+                worker(name)
+            }()
+        }
+    }
+    wg.Wait()
+    
+    cmd3 := exec.Command("sudo", "ffmpeg", "-y", "-f", "concat", "-safe", "0", "-i", "temp/list.ffcat", "-c", "copy", "output.mp4")
+    stdout.Reset()
+    stderr.Reset()
+    cmd3.Stdout = &stdout
+    cmd3.Stderr = &stderr
+    if (cmd3.Run() != nil) {
+        fmt.Fprintln(os.Stderr, stderr.String())
+    }
+    
+    items, _ = ioutil.ReadDir("./temp")
+    for _, item := range items {
+        name := item.Name()
+        err := os.Remove("temp/"+name)
+        if err != nil {
+            fmt.Fprintln(os.Stderr, err)
+        }
+    }
+    
+    fmt.Println("Done")
+}
